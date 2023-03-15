89eabca3375fc6b8e15525c2dcb79f1cf6653bbc
CHSC-1053 <chsaichaitanya1@gmail.com>
2023-03-15 23:10:30 +0530

    updated 1_caesar_cipher.py

--- sem-6_information_security_lab/1_caesar_cipher.py
+++ sem-6_information_security_lab/1_caesar_cipher.py
@@ -1,13 +1,39 @@
-s = input("Enter a string to encrypt: ")
-k = 3
-ki = input("Enter key (default = 3 [Press Enter]): ")
-if ki!="":
-  k = int(ki)
-for i in s:
-    if i==" ":
-       print(" ",end="")
-       continue
-    p = ord(i)-97
-    c = (p + k)%26
-    print(chr(c+97),end="")
-print()
+# This program demonstrates CAESAR CIPHER.
+# Only uppercase, lowercase letters & spaces are allowed.
+plainText = input("Enter a string: ")
+key = 3
+k = input("Enter key (default = 3 [Press Enter]): ")
+if k != "":
+    key = int(k)
+
+# Encryption
+cipherText = ""
+for i in plainText:
+    if i == " ":
+        cipherText += " "
+        continue
+    if 97 <= ord(i):
+        p = ord(i)-97
+        c = (p + key) % 26
+        cipherText += chr(c+97)
+    else:
+        p = ord(i)-65
+        c = (p + key) % 26
+        cipherText += chr(c+65)
+print(cipherText)
+
+# Decryption
+decryptedText = ""
+for i in cipherText:
+    if i == " ":
+        decryptedText += " "
+        continue
+    if 97 <= ord(i):
+        p = ord(i)-97
+        c = (p - key) % 26
+        decryptedText += chr(c+97)
+    else:
+        p = ord(i)-65
+        c = (p - key) % 26
+        decryptedText += chr(c+65)
+print(decryptedText)
