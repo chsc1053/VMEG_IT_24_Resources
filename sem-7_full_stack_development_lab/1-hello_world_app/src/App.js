d04c1d5b90a843719448cc54597bd650a90973e3
CHSC-1053 <chsaichaitanya1@gmail.com>
2023-09-10 23:06:31 +0530

    updated sem-7_full_stack_development_lab

--- sem-7_full_stack_development_lab/1-hello_world_app/src/App.js
+++ sem-7_full_stack_development_lab/1-hello_world_app/src/App.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      Hello, World!
+    <div>
+      <h1>Hello, World!</h1>
     </div>
   );
 }
