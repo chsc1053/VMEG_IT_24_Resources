d9f4507bceb35b996ad6a6e0832855c0992806f9
CHSC-1053 <chsaichaitanya1@gmail.com>
2023-11-15 01:12:28 +0530

    updated sem-7_full_stack_development_lab

--- sem-7_full_stack_development_lab/6-user_login_system/index.js
+++ sem-7_full_stack_development_lab/6-user_login_system/index.js
@@ -1,17 +1,19 @@
-const express = require('express'); 
-const app = express(); 
+const express = require("express");
+const app = express();
 
-const port = 5000; 
+const port = 5000;
 
-app.get('/', (req, res) => { 
- res.sendFile('index.html', {root: __dirname}); 
-
-});
-app.get('/pfolio.html', (req, res) => { 
- res.sendFile('pfolio.html', {root: __dirname}); 
+app.get("/", (req, res) => {
+  res.sendFile("index.html", { root: __dirname });
 });
 
-app.listen(port, () => { 
-{port}
- console.log(`Now listening on port ${port}`); 
-})
+app.post("/home", (req, res) => {
+  res.sendFile("home.html", { root: __dirname });
+});
+
+app.listen(port, () => {
+  {
+    port;
+  }
+  console.log(`Now listening on port ${port}`);
+});
