4b240daedbe6d8aca30ea496cbed605edda709f5
CHSC-1053 <chsaichaitanya1@gmail.com>
2023-11-15 11:15:14 +0530

    updated sem-7_full_stack_development_lab

--- sem-7_full_stack_development_lab/8-book_store/index.js
+++ sem-7_full_stack_development_lab/8-book_store/index.js
@@ -1,6 +1,8 @@
 const express = require('express'); 
 const app = express();              
-const port = 5000;                  
+const port = 5000;
+
+app.use(express.static('public'));         
 
 app.get('/', (req, res) => {        
     res.sendFile('/index.html', {root: __dirname});       
